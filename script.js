472b76c0d84a85b9d74bad22f9dc51d7eb53ad8b
James Isaac <jkijameskisaac@outlook.com>
2021-07-21 11:45:31 +0100

    ConsoleVariableJS
    var
    let
    const
    Console

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Page sucessfully loaded script.js");
+console.warn("This is your first and only warning");
+console.error("ERROR: Reality breached. This world will implode in...10...");
+
+var website = "https://www.numberedentity.com/";
+var entityName = "Numbered Entity";
+
+console.log (website);
+
+
+
+if(entityName == "Numbered Entity") {
+
+    let apples = 4;
+    const height = "6'2";
+    console.log("I have", apples, "apples");
+ console.log("I am", height);
+
+}
